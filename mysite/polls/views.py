1d3a13414fcb89b873ca21da2833b35a37b8cb0c
briggirl2 <briggirl2@gmail.com>
2020-01-23 01:30:19 +0900

    04. url conf/ FBV(function based view)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -13,15 +14,30 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
