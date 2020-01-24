449413e21fc89a249ca34a5552b4e260bc5cec58
briggirl2 <briggirl2@gmail.com>
2020-01-24 16:47:48 +0900

    07. admin customizing

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+# admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+# admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
